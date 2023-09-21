e5bc33261a0d7bbd0b8d7a4a30b84607ac82505c
jamshid-abdukarimov <jamshid20000521@gmail.com>
2023-09-21 16:10:14 +0500

    navbar, footer, search and main banner completed

---
+++ components/SearchForm.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+const SearchForm = () => {
+  const [search, setSearch] = React.useState("");
+  return (
+    <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
+      <label className="flex-center relative w-full max-w-3xl">
+        <Image
+          src="/magnifying-glass.svg"
+          className="absolute left-8"
+          width={32}
+          height={32}
+          alt="search icon"
+        />
+        <Input
+          placeholder="Search"
+          type="text"
+          className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
+        />
+      </label>
+    </form>
+  );
+};
+
+export default SearchForm;
